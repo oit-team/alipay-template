24eaa13ef005cd887ee1bb469194944ed6451bb8
邱旭 <uxuip@outlook.com>
2023-03-03 19:39:34 +0800

    feat: 封装根据schema生成表单组件

---
+++ src/components/UseSchemaField.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { createSchemaField } from '@formily/vue'
+import {
+  Cascader,
+  Checkbox,
+  DatePicker,
+  FormGrid,
+  FormItem,
+  FormLayout,
+  Input,
+  InputNumber,
+  Password,
+  Radio,
+  Select,
+  Switch,
+  TimePicker,
+  Transfer,
+  Upload,
+} from '@formily/element-plus'
+import type { ISchema, SchemaVueComponents } from '@formily/vue'
+
+const props = defineProps<{
+  components?: SchemaVueComponents
+  scope?: any
+  schema: ISchema
+}>()
+
+const { SchemaField } = createSchemaField({
+  components: {
+    FormItem,
+    Input,
+    Password,
+    Cascader,
+    Checkbox,
+    DatePicker,
+    FormGrid,
+    FormLayout,
+    InputNumber,
+    Radio,
+    Select,
+    Switch,
+    TimePicker,
+    Transfer,
+    Upload,
+    ...props.components,
+  },
+  scope: props.scope,
+})
+</script>
+
+<template>
+  <SchemaField :schema="schema" />
+</template>
