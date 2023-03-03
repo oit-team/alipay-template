315e68fad0b7202f743d6361bf0b7f3749ba1c6c
邱旭 <uxuip@outlook.com>
2023-03-03 19:37:18 +0800

    feat: 简单封装表格列组件与表格操作栏

---
+++ src/components/QueryActionColumn.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ElTableColumn } from 'element-plus'
+import type { ConfigProviderInstance } from 'element-plus'
+
+defineProps<{
+  label?: string
+  width?: string
+  fixed?: boolean | 'left' | 'right'
+}>()
+
+const config: ConfigProviderInstance['$props'] = {
+  size: 'small',
+  button: {
+    autoInsertSpace: false,
+  },
+}
+</script>
+
+<template>
+  <ElTableColumn label="操作" v-bind="$attrs">
+    <ElConfigProvider v-bind="config">
+      <div class="query-action-column">
+        <slot />
+      </div>
+    </ElConfigProvider>
+  </ElTableColumn>
+</template>
+
+<style lang="scss">
+.query-action-column {
+  .el-button+.el-button {
+    margin-left: 6px;
+  }
+}
+</style>
---
+++ src/components/QueryToolbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="query-toolbar">
+    <slot />
+  </div>
+</template>
