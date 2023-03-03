4ea928897f55d05e26ec7680e9b0197df8595f95
邱旭 <uxuip@outlook.com>
2023-03-03 19:38:13 +0800

    feat: 封装页面头组件

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ArrowLeft } from '@element-plus/icons-vue'
+defineProps<{
+  title: string
+  onBack?: () => void
+}>()
+</script>
+
+<template>
+  <ElPageHeader
+    class="pl-1"
+    :icon="ArrowLeft"
+    @back="onBack ? onBack() : $router.back()"
+  >
+    <template #content>
+      <span class="text-lg">{{ title }}</span>
+    </template>
+    <template #extra>
+      <slot name="extra" />
+    </template>
+  </ElPageHeader>
+</template>
