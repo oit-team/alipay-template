22007e67c16c4994f8c4e22318c63d753485e651
邱旭 <uxuip@outlook.com>
2023-03-08 14:14:30 +0800

    feat: 详情组件

---
+++ src/components/Descriptions.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface MapItem {
+  prop: string
+  label: string
+  default?: string
+  type?: 'text'
+}
+
+defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+  map: {
+    type: Array as PropType<MapItem[]>,
+    required: true,
+  },
+  defaultText: String,
+})
+</script>
+
+<template>
+  <ElDescriptions v-bind="$attrs">
+    <ElDescriptionsItem v-for="item of map" :key="item.prop" :label="item.label">
+      {{ data?.[item.prop] ?? item?.default ?? defaultText }}
+    </ElDescriptionsItem>
+  </ElDescriptions>
+</template>
