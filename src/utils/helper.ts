043e0b6a5215712753ecdd7b4d59cbf9ee554e90
邱旭 <uxuip@outlook.com>
2023-03-09 10:34:20 +0800

    feat: mergeColumns

---
+++ src/utils/helper.ts
@@ -0,0 +1,15 @@
+import type { TableColumn } from '@uxuip/element-plus-query'
+
+type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export function mergeColumns<T extends TableColumn[]>(targetColumns: T, columns: Record<string, Optional<TableColumn, 'prop'>>) {
+  return targetColumns.map((item) => {
+    if (item.prop && columns[item.prop]) {
+      return {
+        ...item,
+        ...columns[item.prop],
+      }
+    }
+    return item
+  })
+}
