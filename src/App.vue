dbc2ec98302152c3df4b64811f0110c3a04fe201
邱旭 <uxuip@outlook.com>
2023-03-10 17:42:52 +0800

    feat: 整理样式文件

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,3 @@ const config: ConfigProviderInstance['$props'] = {
     <RouterView />
   </ElConfigProvider>
 </template>
-
-<style lang="scss">
-.query-provide {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  flex: 1;
-  gap: 12px;
-
-  .el-date-editor {
-    --el-date-editor-width: auto;
-    --el-date-editor-monthrange-width: auto;
-    --el-date-editor-daterange-width: auto;
-    --el-date-editor-datetimerange-width: auto;
-  }
-
-  .formily-element-plus-form-item-feedback-layout-loose {
-    margin-bottom: 12px;
-  }
-}
-</style>
