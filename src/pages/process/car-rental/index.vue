63d848b6104385a25e530c34c7430360f19be643
邱旭 <uxuip@outlook.com>
2023-03-13 19:34:41 +0800

    feat: 完成租车申请的第一步表单页面

---
+++ src/pages/process/car-rental/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Step1 from './components/step-1/index.vue'
+
+const state = reactive({
+  step: 1,
+})
+
+const view = computed(() => [Step1][state.step - 1])
+</script>
+
+<template>
+  <Component :is="view" />
+</template>
