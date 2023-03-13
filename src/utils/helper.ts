112dd3376665edd78943262bdb92c94495883e64
邱旭 <uxuip@outlook.com>
2023-03-13 19:33:16 +0800

    feat: 新增transformResponse方法，便于格式化响应数据

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,4 @@
+import type { AxiosResponseTransformer } from 'axios'
 import type { TableColumn } from '@uxuip/element-plus-query'
 
 type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
@@ -12,4 +13,14 @@ export function mergeColumns<T extends TableColumn[]>(targetColumns: T, columns:
     }
     return item
   })
+}
+
+export function transformResponse(transformer: AxiosResponseTransformer | AxiosResponseTransformer[]): AxiosResponseTransformer[] {
+  const transformerArray = Array.isArray(transformer) ? transformer : [transformer]
+  const defaultTransformer = axios.defaults.transformResponse
+
+  if (defaultTransformer)
+    transformerArray.unshift(...Array.isArray(defaultTransformer) ? defaultTransformer : [defaultTransformer])
+
+  return transformerArray
 }
