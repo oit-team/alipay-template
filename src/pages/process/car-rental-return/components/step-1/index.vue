b0a55d49f1b5553eb89fb552632b64d179a94092
邱旭 <uxuip@outlook.com>
2023-03-15 15:08:27 +0800

    feat: 完成退车过程第一步页面

---
+++ src/pages/process/car-rental-return/components/step-1/index.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import schema from './schema/form.json'
+import { transformResponse } from '@/utils/helper'
+
+const vehicleId = ref()
+
+console.log(schema)
+
+function executeQuery(fn: any, data: any) {
+  fn({
+    data: {
+      pageNum: 1,
+      pageSize: 10,
+      ...data,
+    },
+  })
+}
+
+const {
+  data: vehicleList,
+  isLoading: vehicleLoading,
+  execute: getVehicleList,
+} = useAxios(
+  '/vehicle/vehicle/getVehicleList',
+  { transformResponse: transformResponse(data => data.body.vehicleList) },
+  { immediate: false },
+)
+
+const form = createForm({})
+
+const log = console.log
+</script>
+
+<template>
+  <FormProvider :form="form">
+    <div class="flex flex-col gap-2 p-2">
+      <PageHeader title="申请退车">
+        <template #extra>
+          <Submit type="primary" @submit="log">
+            提交
+          </Submit>
+        </template>
+      </PageHeader>
+
+      <ElCard>
+        <template #header>
+          <div>
+            <span>车牌号：</span>
+            <ElSelect
+              v-model="vehicleId"
+              default-first-option
+              filterable
+              :loading="vehicleLoading"
+              placeholder="请输入内容搜索"
+              remote
+              :remote-method="(v: any) => v && executeQuery(getVehicleList, { licensePlateNumber: v })"
+              remote-show-suffix
+              reserve-keyword
+            >
+              <ElOption
+                v-for="item in vehicleList"
+                :key="item.vehicleId"
+                :label="`${item.licensePlateNumber}`"
+                :value="item.vehicleId"
+              />
+            </ElSelect>
+          </div>
+        </template>
+        <Descriptions
+          border
+          :data="vehicleList?.find((item: any) => item.vehicleId === vehicleId)"
+          default-text="暂无"
+          label-width="130px"
+          :options="[
+            { label: '车架号', prop: 'vehicleFrameNumber' },
+            { label: '司机姓名', prop: 'city' },
+            { label: '身份证号', prop: 'brandSeries' },
+            { label: '所属运营商', prop: 'bodyColor' },
+            { label: '起租日期', prop: 'mileage' },
+            { label: '到租日期', prop: 'endTime' },
+          ]"
+        />
+      </ElCard>
+
+      <ElCard header="退车信息">
+        <div class="pt-5">
+          <FormLayout
+            label-col="5"
+          >
+            <UseSchemaField :schema="schema" />
+          </FormLayout>
+        </div>
+      </ElCard>
+    </div>
+  </FormProvider>
+</template>
+
+<style scoped>
+.el-card {
+  --el-card-padding: 0;
+}
+
+:deep(.el-card__header) {
+  --el-card-padding: 12px;
+}
+</style>
