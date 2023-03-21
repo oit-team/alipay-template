b1f49a4a8adab371bee38442b338d380b8c4790b
邱旭 <uxuip@outlook.com>
2023-03-21 11:35:23 +0800

    feat: 表单上传验证，确保文件已上传

--- src/plugins/formily.ts
+++ src/plugins/formily.ts
@@ -1,12 +1,22 @@
 import { registerValidateLocale, registerValidateRules, setValidateLanguage } from '@formily/core'
+import type { UploadFile } from 'element-plus'
 
 setValidateLanguage('zh-CN')
 
 // 自定义校验提示
 registerValidateLocale({
   'zh-CN': {
-    required: '请填写此项',
+    required: '请填写必填项',
   },
 })
 
-registerValidateRules({})
+// 自定义校验规则
+registerValidateRules({
+  upload: (fileList?: UploadFile[]) => {
+    if (!fileList)
+      return true
+
+    const success = fileList.every(file => file.status === 'success')
+    return success || '请等待文件上传完成'
+  },
+})
