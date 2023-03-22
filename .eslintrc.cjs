5087217701a23828d7645f8105316f24718730ce
邱旭 <uxuip@outlook.com>
2023-03-22 14:23:21 +0800

    chore: 提交时检测console.log

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+const production = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  extends: '@uxuip',
+  rules: {
+    'no-console': production ? 'error' : 'warn',
+    '@typescript-eslint/prefer-ts-expect-error': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+      },
+    ],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'UNIQUE',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'SLOT',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: true,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-debugger': 'warn',
+    'no-restricted-syntax': 'warn',
+  },
+}
