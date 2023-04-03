7a6ecd624e8b0d3b55a7dff497f55cd27e7fb499
刘运双 <1048821976@qq.com>
2023-04-03 16:21:49 +0800

    feat: 车辆详情-租赁订单新增T3

---
+++ src/pages/lease/car/info/components/Self-support.vue
@@ -0,0 +1,144 @@
+<route lang="yaml">
+meta:
+  cache: true
+      </route>
+
+<script setup lang="ts">
+import { vehicleParamsSymbol } from '../types'
+
+const queryRef = ref()
+const vehicleParams = inject(vehicleParamsSymbol)
+const vehicleId = vehicleParams?.vehicleId
+
+onMounted(() => {
+  queryRef.value?.query()
+})
+
+const columns = [
+  {
+    prop: 'leaseOrderNo',
+    label: '订单编号',
+  },
+  {
+    prop: 'orderStatueName',
+    label: '订单状态',
+  },
+  {
+    prop: 'driverName',
+    label: '司机姓名',
+  },
+  {
+    prop: 'licensePlateNumber',
+    label: '车牌号',
+  },
+  {
+    prop: 'vinNo',
+    label: '车架号',
+  },
+  {
+    prop: 'caseCode',
+    label: '方案编号',
+  },
+  {
+    prop: 'rent',
+    label: '租金（元）',
+  },
+  {
+    prop: 'cashPledge',
+    label: '押金（元）',
+  },
+  {
+    prop: 'startTime',
+    label: '起租日期',
+  },
+  {
+    prop: 'endTime',
+    label: '终止时间',
+  },
+  {
+    prop: 'createTime',
+    label: '创建时间',
+  },
+  {
+    prop: 'updateTime',
+    label: '更新时间',
+  },
+]
+const columnsConfig = {
+  caseCode: {
+    width: 150,
+  },
+  leaseTerm: {
+    width: 150,
+  },
+  cashPledge: {
+    minWidth: 150,
+  },
+  updateTime: {
+    width: 150,
+  },
+  vinNo: {
+    width: 150,
+  },
+  rent: {
+    minWidth: 150,
+  },
+  leaseOrderNo: {
+    width: 150,
+  },
+  licensePlateNumber: {
+    width: 150,
+  },
+  createTime: {
+    width: 150,
+  },
+  driverName: {
+    minWidth: 150,
+  },
+  startTime: {
+    width: 150,
+  },
+  orderStatue: {
+    width: 150,
+  },
+  orderStatueName: {
+    width: 150,
+  },
+  endTime: {
+    width: 150,
+  },
+}
+</script>
+
+<template>
+  <div class="h-full p-2">
+    <UseQuery
+      v-slot="attrs"
+      :columns="columns"
+      :columns-config="columnsConfig"
+      :data="{
+        vehicleId,
+      }"
+      :key-map="{ data: 'resultList', total: 'totalCount' }"
+      url="/order/leaseOrder/getLeaseOrderList"
+    >
+      <QueryProvide
+        v-bind="attrs"
+        ref="queryRef"
+        auto-query="active"
+      >
+        <QueryTable>
+          <template #actions>
+            <QueryActionColumn v-slot="{ row }" fixed="right" label="操作" width="100px">
+              <ElButton size="small" type="success" @click="$router.push(`/lease/order/info/self-support/${row.id}`)">
+                {{ $t('button.info') }}
+              </ElButton>
+            </QueryActionColumn>
+          </template>
+        </QueryTable>
+        <QueryPagination />
+      </QueryProvide>
+    </UseQuery>
+  </div>
+</template>
+
---
+++ src/pages/lease/car/info/components/T3.vue
@@ -0,0 +1,184 @@
+<route lang="yaml">
+meta:
+  cache: true
+    </route>
+
+<script setup lang="ts">
+const queryRef = ref()
+
+onMounted(() => {
+  queryRef.value?.query()
+})
+
+// 表格的json
+const columns = [
+  {
+    prop: 'orderNo',
+    label: '订单编号',
+  },
+  {
+    prop: 'orderType',
+    label: '订单类型',
+  },
+  {
+    prop: 'assetCompany',
+    label: '资产公司',
+  },
+  {
+    prop: 'city',
+    label: '城市',
+  },
+  {
+    prop: 'leasingCity',
+    label: '司机ID',
+  },
+  {
+    prop: 'driverName',
+    label: '司机姓名',
+  },
+  {
+    prop: 'driverMobileNumber',
+    label: '司机手机号',
+  },
+  {
+    prop: 'orderStatus',
+    label: '订单状态',
+  },
+  {
+    prop: 'brandCarModel',
+    label: '品牌车系车型',
+  },
+  {
+    prop: 'carNumber',
+    label: '车牌号',
+  },
+  {
+    prop: 'vinNo',
+    label: '车架号',
+  },
+  {
+    prop: 'schemeNo',
+    label: '方案编号',
+  },
+  {
+    prop: 'supplyMode',
+    label: '供应方式',
+  },
+  {
+    prop: 'tenancy',
+    label: '租期',
+  },
+  {
+    prop: 'monthlyRent',
+    label: '月租',
+  },
+  {
+    prop: 'importUserName',
+    label: '数据导入用户名',
+  },
+  {
+    prop: 'importTime',
+    label: '数据导入时间',
+  },
+  {
+    prop: 't3CreatTime',
+    label: '订单创建时间',
+  },
+  {
+    prop: 'endDate',
+    label: '截至时间',
+  },
+  {
+    prop: 'actualEndDate',
+    label: '实际截至时间',
+  },
+  {
+    prop: 'deliveryDate',
+    label: '交付日期',
+  },
+]
+const columnsConfig = {
+  orderNo: {
+    width: 150,
+  },
+  brandCarModel: {
+    width: 200,
+  },
+  orderType: {
+    minWidth: 100,
+  },
+  t3CreatTime: {
+    width: 200,
+  },
+  actualEndDate: {
+    width: 150,
+  },
+  endDate: {
+    width: 150,
+  },
+  tenancy: {
+    width: 150,
+  },
+  schemeNo: {
+    width: 150,
+  },
+  carNumber: {
+    width: 150,
+  },
+  driverName: {
+    minWidth: 100,
+  },
+  driverMobileNumber: {
+    width: 150,
+  },
+  assetCompany: {
+    width: 150,
+  },
+  vinNo: {
+    width: 200,
+  },
+  deliveryDate: {
+    width: 200,
+  },
+  importUserName: {
+    width: 200,
+  },
+  monthlyRent: {
+    width: 100,
+  },
+  importTime: {
+    width: 200,
+  },
+}
+</script>
+
+<template>
+  <div class="h-full p-2">
+    <UseQuery
+      v-slot="attrs"
+      :columns="columns"
+      :columns-config="columnsConfig"
+      :key-map="{ data: 'resultList', total: 'totalCount' }"
+      url="/order/leaseOrder/getT3LeaseOrderList"
+    >
+      <QueryProvide
+        v-bind="attrs"
+        ref="queryRef"
+        auto-query="active"
+      >
+        <QueryToolbar />
+        <QueryTable>
+          <template #actions>
+            <QueryActionColumn v-slot="{ row }" fixed="right" label="操作" width="100px">
+              <ElButton size="small" type="success" @click="$router.push(`/lease/order/info/t-three/${row.id}`)">
+                {{ $t('button.info') }}
+              </ElButton>
+            </QueryActionColumn>
+          </template>
+        </QueryTable>
+        <QueryPagination />
+      </QueryProvide>
+    </UseQuery>
+  </div>
+</template>
+
