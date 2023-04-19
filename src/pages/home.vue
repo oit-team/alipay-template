683119526590b8d18b6b262f5a40d8be7c9ee940
邱旭 <uxuip@outlook.com>
2023-04-19 18:55:34 +0800

    feat(左侧导航): 提供一级菜单跳转，默认跳转到第一个可用的路由

--- src/pages/home.vue
+++
@@ -1,7 +0,0 @@
-<route>
-redirect: /lease/work-order
-</route>
-
-<template>
-  <div>home</div>
-</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,2 @@
-<route lang="yaml">
-redirect: /home
-</route>
-
 <!-- eslint-disable-next-line vue/valid-template-root -->
 <template />
