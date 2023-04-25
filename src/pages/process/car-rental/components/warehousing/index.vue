afb6e58d19abde0d70315ed4662d0d79d1a4e879
邱旭 <uxuip@outlook.com>
2023-04-25 17:51:31 +0800

    feat(车辆出库): 车辆情况和附件上传

--- src/pages/process/car-rental/components/warehousing/index.vue
+++ src/pages/process/car-rental/components/warehousing/index.vue
@@ -2,40 +2,91 @@
 import DriverInfo from '@/pages/lease/driver/info/[id].vue'
 import VehicleInfo from '@/pages/lease/car/info/[id].vue'
 import { workOrderInfoSymbol, workOrderSubmitSymbol } from '@/pages/process/types'
+import vehicleCondition from '~/pages/process/schema/vehicleCondition.json'
+import Upload from '~/components/FUpload'
+import { useFlowOption } from '@/pages/process/hooks/useFlowOption'
 
 const { t } = useI18n()
 const router = useRouter()
+const flowOption = useFlowOption()
 const workOrderSubmit = inject(workOrderSubmitSymbol)
 const workOrderInfo = inject(workOrderInfoSymbol)
 const isReview = computed(() => workOrderInfo?.value?.isReview)
+const form = createForm()
 
-async function submit() {
-  await workOrderSubmit?.({}, {
+async function submit(formData: any) {
+  await workOrderSubmit?.(formData, {
     approvalStatus: 1,
   })
 
   ElMessage.success(t('submit.success'))
   router.back()
 }
+
+onMounted(async () => {
+  const { data: vehicleOutboundInfo } = await axios.post('/order/leaseOrder/getVehicleOutboundInfo', {
+    workCode: flowOption?.workCode,
+  })
+  form.setInitialValues(vehicleOutboundInfo)
+})
 </script>
 
 <template>
   <div class="h-full flex flex-col">
-    <PageHeader :title="`申请租车-${$route.query?.workCode}`">
-      <template v-if="!isReview" #extra>
-        <ElButton type="primary" @click="submit">
-          提交
-        </ElButton>
-      </template>
-    </PageHeader>
+    <FormProvider :form="form">
+      <PageHeader :title="`申请租车-${$route.query?.workCode}`">
+        <template v-if="!isReview" #extra>
+          <Submit type="primary" @submit="submit">
+            提交
+          </Submit>
+        </template>
+      </PageHeader>
 
-    <ElTabs class="flex-1" type="border-card">
-      <ElTabPane label="司机信息">
-        <DriverInfo :driver-id="workOrderInfo?.mainParams.driverId" inset />
-      </ElTabPane>
-      <ElTabPane label="车辆信息">
-        <VehicleInfo inset :vehicle-id="workOrderInfo?.mainParams.vehicleId" />
-      </ElTabPane>
-    </ElTabs>
+      <ElTabs class="flex-1" type="border-card">
+        <ElTabPane label="附件">
+          <FormLayout class="p-2" label-width="6em">
+            <div class="flex flex-col gap-2">
+              <ElCard header="附件">
+                <div class="grid grid-cols-2 p-2">
+                  <Field
+                    :component="[Upload, {
+                      multiple: true,
+                      format: 'url',
+                      limit: 3,
+                    }]"
+                    :decorator="[FormItem]"
+                    name="driverAnnex"
+                    title="司机附件"
+                  />
+                  <Field
+                    :component="[Upload, {
+                      multiple: true,
+                      format: 'url',
+                      limit: 3,
+                    }]"
+                    :decorator="[FormItem]"
+                    name="resList"
+                    title="车辆附件"
+                  />
+                </div>
+              </ElCard>
+              <ElCard header="车辆情况">
+                <div class="p-2">
+                  <ObjectField name="vehicleSupplementary">
+                    <UseSchemaField :schema="vehicleCondition" />
+                  </ObjectField>
+                </div>
+              </ElCard>
+            </div>
+          </FormLayout>
+        </ElTabPane>
+        <ElTabPane label="司机信息">
+          <DriverInfo :driver-id="workOrderInfo?.mainParams.driverId" inset />
+        </ElTabPane>
+        <ElTabPane label="车辆信息">
+          <VehicleInfo inset :vehicle-id="workOrderInfo?.mainParams.vehicleId" />
+        </ElTabPane>
+      </ElTabs>
+    </FormProvider>
   </div>
 </template>
