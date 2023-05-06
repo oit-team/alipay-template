6d195acd4e9c34b3bb5cd13388f914398e604832
邱旭 <uxuip@outlook.com>
2023-05-06 11:15:49 +0800

    feat: 补充步骤组件类型注释

--- src/pages/process/types.ts
+++ src/pages/process/types.ts
@@ -57,17 +57,29 @@ export interface FlowOption {
   workCode: string
   flowCode: string
   taskCode: string
+  // 当前初始步骤
   step: number
+  // 选中的步骤
   stepActive?: number
+  // 选中的步骤code
   stepCodeActive?: string
+  // 当前操作日志
   currentLogs?: string[]
+  // 当前步骤详细数据
   currentStep?: any
+  // 当前步骤是否时回显
   isReview: boolean
+  // 主要参数
   mainParams: Record<string, any>
+  // 设置选中的步骤
   setViewStep: (item: FlowOption['workFlowSteps'][number]) => void
+  // 提交
   submit: WorkOrderSubmit
+  // 申请
   apply: WorkOrderApply
+  // 拒绝
   reject: (data?: Record<string, any>) => Promise<any>
+  // 步骤列表
   workFlowSteps: {
     id: number
     name: string
