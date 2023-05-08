0a873fffdb43f153003945fcd76941278b2d7a8b
李晓飞 <xiaofeiyu187@163.com>
2023-05-08 18:01:18 +0800

    feat: 新增换车记录列表，修改订单换车页面

---
+++ src/pages/lease/change-records/index.vue
@@ -0,0 +1,38 @@
+<route lang="yaml">
+meta:
+  cache: true
+  </route>
+
+<script lang="ts" setup>
+const queryRef = ref()
+</script>
+
+<template>
+  <div class="h-full p-2">
+    <UseQuery
+      v-slot="attrs"
+      columns-key="1683518035995"
+      :key-map="{ data: 'result', total: 'totalCount' }"
+      schema-key="1683518111791"
+      url="/order/leaseOrder/getVehicleChangeList"
+    >
+      <QueryProvide
+        v-bind="attrs"
+        ref="queryRef"
+        auto-query="active"
+      >
+        <QueryForm />
+        <QueryTable>
+          <template #actions>
+            <QueryActionColumn v-slot="{ row }" label="操作" width="100px">
+              <ElButton size="small" type="info" @click="$router.push(`./order/transfer/${row.recordsId}?hide=1`)">
+                详情
+              </ElButton>
+            </QueryActionColumn>
+          </template>
+        </QueryTable>
+        <QueryPagination />
+      </QueryProvide>
+    </UseQuery>
+  </div>
+</template>
