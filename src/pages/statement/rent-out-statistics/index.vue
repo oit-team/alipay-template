6767178e0b336313ac26d46aea12e3da7a6928f4
邱旭 <uxuip@outlook.com>
2023-05-10 13:38:09 +0800

    feat(退租统计): 导入T3代扣数据

--- src/pages/statement/rent-out-statistics/index.vue
+++ src/pages/statement/rent-out-statistics/index.vue
@@ -6,9 +6,11 @@
 <script setup lang="ts">
 import { downloadFile } from '@oit/utils'
 import querySchema from './schema/query.json'
+import { importNotice } from '@/utils/importNotice'
 
 const columns = [
   { prop: 'licensePlateNumber', label: '车牌' },
+  { prop: 'driverNo', label: '司机ID' },
   { prop: 'driverName', label: '司机名称' },
   { prop: 'leaseTerm', label: '租期' },
   { prop: 'rent', label: '租金' },
@@ -20,7 +22,6 @@ const columns = [
   { prop: 'dailyRent', label: '日租金' },
   { prop: 'rentReceivable', label: '应收租金' },
   { prop: 't3Withholding', label: 'T3代扣' },
-  { prop: 'driverNo', label: '司机ID' },
 ]
 
 const queryData = ref()
@@ -33,6 +34,34 @@ async function downloadExport() {
   )
   downloadFile(data, '退租统计.xls')
 }
+
+const { open, reset, onChange } = useFileDialog()
+
+onChange(async (files) => {
+  if (!files?.length)
+    return
+
+  try {
+    const { profile } = useUserStore()
+    const { data } = await axios.post('/order/leaseOrder/importWithholdingRecords', {
+      file: files[0],
+      userId: profile?.userId,
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+
+    importNotice(data.importIndex)
+  }
+  catch (error) {
+    ElMessageBox.alert((error as any).message, '警告', {
+      type: 'warning',
+    })
+  }
+
+  reset()
+})
 </script>
 
 <template>
@@ -54,6 +83,9 @@ async function downloadExport() {
         <QueryToolbar>
           <TButton icon="i-export" @click="downloadExport()">
             导出
+          </TButton>
+          <TButton icon="i-import" @click="open()">
+            导入T3代扣信息
           </TButton>
         </QueryToolbar>
         <QueryTable />
