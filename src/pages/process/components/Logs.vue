7eb88ae775b6a720c98a919e1a97a34465dbeba4
邱旭 <uxuip@outlook.com>
2023-05-12 13:58:52 +0800

    fix: 修改流程日志显示逻辑，仅在有确认人时显示

--- src/pages/process/components/Logs.vue
+++ src/pages/process/components/Logs.vue
@@ -2,16 +2,16 @@
 import { useFlowOption } from '../hooks/useFlowOption'
 
 const showLogs = ref(false)
-const flowOption = useFlowOption()
+const flowOption = useFlowOption()!
 </script>
 
 <template>
-  <div v-if="flowOption?.currentLogs?.[0]" class="p-2" v-bind="$attrs">
+  <div v-if="flowOption.currentStep?.handleUser" class="p-2" v-bind="$attrs">
     <ElCard>
       <div class="flex items-center justify-between p-2">
         <div class="flex gap-10">
-          <span>确认人：{{ flowOption?.currentStep.handleUser }}</span>
-          <span>确认时间：{{ flowOption?.currentStep.handleTime }}</span>
+          <span>确认人：{{ flowOption.currentStep.handleUser }}</span>
+          <span>确认时间：{{ flowOption.currentStep.handleTime }}</span>
         </div>
         <ElButton @click="showLogs = true">
           查看全部
@@ -21,7 +21,7 @@ const flowOption = useFlowOption()
   </div>
   <ElDrawer v-model="showLogs" title="操作日志">
     <div class="p-2 flex flex-col gap-2">
-      <ElCard v-for="(item, index) of flowOption?.currentLogs" :key="index" class="p-2">
+      <ElCard v-for="(item, index) of flowOption.currentLogs" :key="index" class="p-2">
         {{ item }}
       </ElCard>
     </div>
